c7d7fea115125e716792ffb28278b0bd9fe94d61
Ren <cocacola@live.jp>
2023-09-06 23:49:25 +0900

    ocr初挑戦

---
+++ test1/moji.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import pyocr
+
+pyocr.tesseract.TESSERACT_CMD = r'C:\Users\cocac\PycharmProjects\ocr_table\venv\Lib\site-packages\pyocr\tesseract.py'
+
+# OCR Engine 取得
+engines = pyocr.get_available_tools()
+print(engines)
+engines = engines[0]
+
+# 画像読み込み
+txt = engines.image_to_string(Image.open('moji_en.png'), lang='eng')
+print(txt)
